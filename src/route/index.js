7d0e62be00c05a6ca66f38ad90c80148c48b7e85
loudmilakiki <120623945+loudmilakiki@users.noreply.github.com>
2023-09-24 11:54:44 +0300

    init calc

--- src/route/index.js
+++ src/route/index.js
@@ -3,11 +3,15 @@ const express = require('express')
 const router = express.Router()
 
 // Підключіть файли роутів
-const test = require('./test')
+//const test = require('./test')
 // Підключіть інші файли роутів, якщо є
+//const test2 = require('./test2')
 
+const calc = require('./calc')
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+// router.use('/', test)
+// router.use('/test', test2)
+router.use('/calc', calc)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
